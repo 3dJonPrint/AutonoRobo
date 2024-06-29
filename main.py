5767c4f5d2a9167da1da9a9c7b0b73b7b44768bf
Phillip Schmie <34949466-phillip39@users.noreply.replit.com>
2024-06-29 14:32:18 +0000

    push

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import time
 #Motor A
 motorForwardLeftPin = 26
 motorReverseLeftPin = 19
+motorSpeedLeftPin = 
   
 #Motor B
 motorForwardRightPin = 13
 motorReverseRightPin = 6
+motorSpeedRightPin = 
 
 forwardLeft = gpiozero.PWMOutputDevice(motorForwardLeftPin, True, 0, 1000)
 reverseLeft = gpiozero.PWMOutputDevice(motorReverseLeftPin, True, 0, 1000)
+speedLeft = gpiozero.PWMOutputDevice(motorSpeedLeftPin, True, 0, 1000)
 
 forwardRight = gpiozero.PWMOutputDevice(motorForwardRightPin, True, 0, 1000)
 reverseRight = gpiozero.PWMOutputDevice(motorReverseRightPin, True, 0, 1000)
+speedRight = gpiozero.PWMOutputDevice(motorSpeedRightPin, True, 0, 1000)
 
 def stop():
   forwardLeft.value = 0
@@ -27,5 +31,8 @@ def forwardDrive():
   forwardRight.value = 1.0
   reverseRight.value = 0
 
-def steer():
-  
+def main():
+  forwardDrive()
+  time.sleep(5)
+  stop()
+  time.sleep(5)

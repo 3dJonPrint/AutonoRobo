d37701136900514ebe86c1d5849896913ae76ca9
3dJonPrint <167607496+3dJonPrint@users.noreply.github.com>
2024-06-29 13:40:25 +0000

    push

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hi")
+import gpiozero
+import time
+
+#Motor A
+motorForwardLeftPin = 26
+motorReverseLeftPin = 19
+  
+#Motor B
+motorForwardRightPin = 13
+motorReverseRightPin = 6
+
+forwardLeft = gpiozero.PWMOutputDevice(motorForwardLeftPin, True, 0, 1000)
+reverseLeft = gpiozero.PWMOutputDevice(motorReverseLeftPin, True, 0, 1000)
+
+forwardRight = gpiozero.PWMOutputDevice(motorForwardRightPin, True, 0, 1000)
+reverseRight = gpiozero.PWMOutputDevice(motorReverseRightPin, True, 0, 1000)
+
+def stop():
+  forwardLeft.value = 0
+  forwardRight.value = 0
+  reverseLeft.value = 0
+  reverseRight.value = 0
+
+def forwardDrive():
+  forwardLeft.value = 1.0
+  reverseLeft.value = 0
+  forwardRight.value = 1.0
+  reverseRight.value = 0
+
+def steer():
+  

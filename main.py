ae432208b747bf3202bd61c49f9daebbdf7541b2
3dJonPrint <167607496+3dJonPrint@users.noreply.github.com>
2024-06-30 10:29:23 +0000

    push

--- main.py
+++ main.py
@@ -96,9 +96,7 @@ def linesensordrive():
   lr = lineSensorRightPin.line_detected
   #ultraschallSensor.when_in_range(turnAround)
   if (not ll and lm and not lr):
-    drive(0.3, 0)
-    time.sleep(0.1)
-    drive(0.2, 0)
+    drive(0.25, 0)
     print("mid")
   elif ( ll and lm and lr):
     drive(0.1, 0)
